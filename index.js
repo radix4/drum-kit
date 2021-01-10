1bffdf199f2254f440ca2bc4b1eadf319c1848d3
Thang Cao <shiyoline@gmail.com>
2021-01-10 10:15:15 -0800

    add css effect on buttons

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ window.addEventListener('keydown', playSound)
 function playSound(event) {
   // parameter targets the whole tag
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
-  console.log(key)
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
 
   if (!audio) return // no key found
 
   audio.currentTime = 0
   audio.play()
+  key.classList.add('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return // skip other properties, only focus transform
+  this.classList.remove('playing') // this removes to the key element
 }
